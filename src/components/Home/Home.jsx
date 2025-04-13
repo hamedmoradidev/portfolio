a0bd386c5bfecae6b24a96833e21a4555be78601
Hamed Moradi <123096117+hamedmoradidev@users.noreply.github.com>
2025-04-13 07:11:29 +0330

    Add files via upload

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,13 +8,13 @@ import me2 from './images/me2.png'
 export default function Home() {
   return (
     <>
-        <div id='home' className='bg-project-light-gray-home h-lvh text-project-black-first flex justify-start flex-wrap items-center relative overflow-hidden'>
+        <div id='home' className='bg-project-light-gray-home h-lvh text-project-black-first flex justify-start flex-wrap items-center align-middle relative overflow-hidden'>
         <div className='bg-project-black-first h-[2000px] w-[1500px] lg:absolute lg:left-2/5 lg:-top-20 lg:rotate-10 lg:transform-3d'>
         </div>
         <Nav />
         <Content />
         <img className='hidden lg:block z-20 h-4/5 absolute bottom-0 right-40' src={me1} alt="" />
-        <img className='lg:hidden z-20 h-4/5 absolute bottom-0 right-0' src={me2} alt="" />
+        <img className='lg:hidden z-20 h-4/5 absolute bottom-0 right-0 object-contain' src={me2} alt="" />
         </div>
     </>
   )
